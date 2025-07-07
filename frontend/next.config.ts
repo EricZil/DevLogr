c4dc669ad479a8a89c2a5205afa4117cf966b586
Eric Zilvytis <eric@nezser.com>
2025-07-07 18:44:28 +0300

    Let's pray

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -9,7 +9,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    esmExternals: 'loose',
+  },
   webpack: (config, { isServer }) => {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
     if (!isServer) {
       config.resolve.fallback = {
         ...config.resolve.fallback,
